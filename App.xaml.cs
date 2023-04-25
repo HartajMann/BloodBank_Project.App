1939ef59abd31bdffc3bcee2f7bb955f376f8695
Hartaj Singh Mann <99726645+HartajMann@users.noreply.github.com>
2023-04-25 12:45:10 -0600

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace BloodBank_Project;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BloodBank_Project;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
