ae93e60f03468b141b6069792b919fc1b29a6b0e
Hartaj Singh Mann <99726645+HartajMann@users.noreply.github.com>
2023-04-25 22:14:10 -0600

    Login Page complete(With Exceptions)

---
+++ Data/InvalidLoginException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodBank_Project.Data
+{
+    public class InvalidLoginException:Exception
+    {
+        public InvalidLoginException() :base("Entered Username or Password is Incorrect."){ }
+
+        public InvalidLoginException(string message) : base(message) { }
+
+    }
+}
