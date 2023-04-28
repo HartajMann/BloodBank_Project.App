e0aeb63c71b379ace562ebf96b8e9ebf589c469f
Hartaj Singh Mann <99726645+HartajMann@users.noreply.github.com>
2023-04-27 19:58:16 -0600

    Application Complete

---
+++ Data/InvalidDonationException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodBank_Project.Data
+{
+    public class InvalidDonationException:Exception
+    {
+        public InvalidDonationException() : base("") { }
+
+        public InvalidDonationException(string message) : base(message) { }
+    }
+}
